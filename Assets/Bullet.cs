5c388fa35db1989d3de4d118ac4ebf9f720f67f0
Senzaj <sanamistik@mail.ru>
2023-10-31 23:06:26 +0300

    features

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -6,39 +6,42 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Rigidbody2D BirdRigid; 
     [SerializeField] public Rigidbody2D ShootRigid;
 
-    private const float maxDistance = 3f;
+    private const float maxDistance = 5f;
 
     private bool isPressed;
 
     private void Update()
     {
-        if (isPressed == true)
+        if (isPressed)
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.touches[0].position); 
+            Vector2 touch = Camera.main.ScreenToWorldPoint(Input.touches[0].position); 
 
-            if (Vector2.Distance(mousePos, ShootRigid.position) > maxDistance) 
+            if (Vector2.Distance(touch, ShootRigid.position) > maxDistance) 
             {
-                BirdRigid.position = ShootRigid.position + (mousePos - ShootRigid.position).normalized * maxDistance;
+                BirdRigid.position = ShootRigid.position + (touch - ShootRigid.position).normalized * maxDistance;
             }
             else
             {
-                BirdRigid.position = mousePos;
+                BirdRigid.position = touch;
             }
         }
     }
 
     private void OnMouseDown()
     {
+        BirdRigid.velocity = Vector2.zero;
         isPressed = true;
         BirdRigid.isKinematic = true;
+        BirdRigid.useFullKinematicContacts = true;
     }
 
     private void OnMouseUp()
     {
         isPressed = false;
+        BirdRigid.useFullKinematicContacts = false;
         BirdRigid.isKinematic = false;
 
-        StartCoroutine(LetGo());
+        //StartCoroutine(LetGo());
     }
 
     IEnumerator LetGo()
@@ -46,7 +49,7 @@ public class Bullet : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
 
         gameObject.GetComponent<SpringJoint2D>().enabled = false;
-        this.enabled = false; 
+        enabled = false; 
         Destroy(gameObject, 5); 
 
         yield return new WaitForSeconds(2);
